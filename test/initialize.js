901765d085fb3b5cdfc5a2b6c9fc4094864e11db
rajikaimal <inbox.rajika@gmail.com>
2016-08-26 10:58:51 +0530

    initialize command test

--- test/initialize.js
+++ test/initialize.js
@@ -1,7 +1,19 @@
 const assert = require('chai').assert;
+const init = require('../lib/init');
+const fs = require('fs.extra');
+const rmdir = require('rimraf');
 
 describe('initialize React application', function() {
+	
 	it('should initialize react app', function(done) {
-		done();
+		init('../test-project', undefined, function(result) {
+			rmdir('../test-project', err => {
+				if(err) throw new Error('failed');
+				else {
+					done();
+				}
+			});
+		});
 	});
+
 });
