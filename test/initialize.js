7a2b84fb21e18a267eaebe72bdadf3b6b1c3767d
rajikaimal <inbox.rajika@gmail.com>
2016-08-26 14:16:17 +0530

    cleanup

--- test/initialize.js
+++ test/initialize.js
@@ -1,10 +1,8 @@
 const assert = require('chai').assert;
 const init = require('../lib/init');
-const fs = require('fs.extra');
 const rmdir = require('rimraf');
 
 describe('initialize React application', function() {
-	
 	it('should initialize react app', function(done) {
 		init('../test-project', undefined, function(result) {
 			rmdir('../test-project', err => {
@@ -23,10 +21,9 @@ describe('initialize React application', function() {
 				if(err) throw new Error('failed');
 				else {
 					assert.equal(result, true);
-					done();
+          done();
 				}
 			});
 		});
 	});
-
 });
