8f5cfc913be1ef77e7f013a2e2c6af8f7dd280bb
rajikaimal <inbox.rajika@gmail.com>
2016-08-26 14:27:27 +0530

    linter: cleanup

--- test/generate.js
+++ test/generate.js
@@ -3,21 +3,21 @@ const create = require('../lib/generate').create;
 
 describe('Create react components', function() {
 	it('should create a parent react component', function(done) {
-    create('core', 'sample', 'child', function(res) {
-      assert.equal(res, true);
-      done();
-    });
+	  create('core', 'sample', 'child', function(res) {
+	    assert.equal(res, true);
+	    done();
+	  });
 	});
 	it('should not create a parent react component', function(done) {
-    create('core', 'sample', 'somethingelse', function(res) {
-      assert.equal(res, false);
-      done();
-    });
+	  create('core', 'sample', 'somethingelse', function(res) {
+	    assert.equal(res, false);
+	    done();
+	  });
 	});
 	it('should not create a react component', function(done) {
-    create('non-existing-module', 'sample', 'parent', function(res) {
-      assert.equal(res, false);
-      done();
-    });
+	  create('non-existing-module', 'sample', 'parent', function(res) {
+	    assert.equal(res, false);
+	    done();
+	  });
 	});
 });
