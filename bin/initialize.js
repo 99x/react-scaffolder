4ed5fd91ea5ae80fd53264af34ea57ca4df213a1
rajikaimal <inbox.rajika@gmail.com>
2016-08-30 14:58:29 +0530

    add options for child, parent component generation

--- bin/initialize.js
+++ bin/initialize.js
@@ -2,6 +2,7 @@
 
 const program = require('commander');
 const init = require('../lib/init');
+const generate = require('../lib/generate');
 const packageVersion = require('../package.json').version;
 
 /**
@@ -19,13 +20,12 @@ program
   .description('initialize React project')
   .option("-l, --eslint", "eslint required or not ?")
   .action(function(projectname, options) {
-    console.log('init "%s" using %s mode', projectname, options.eslint);
     if (projectname === undefined) {
       console.log('provide a project name');
       return;
     }
     else
-    {
+		{
 			init(projectname, options.eslint, function initProject(res) {
 				if(res) {
 					console.log('application created successfully');
@@ -34,7 +34,7 @@ program
 					console.log('something went wrong !');
 				}
 			});
-    }
+		}
   }).on('--help', function() {
     console.log('  Examples:');
     console.log();
@@ -42,6 +42,40 @@ program
     console.log('    $ react-cli init -l awesomereact');
     console.log();
   });
+
+/**
+ * command for generating a react component
+ */
+program
+  .command('generate [modulename] [name]')
+  .alias('g')
+  .description('generate a react component')
+  .option("-p, --parent", "parent component")
+  .option("-c, --child", "child component")
+  .action(function(modulename, name, options) {
+    console.log(modulename, name);
+    if(modulename !== undefined && name !== undefined && options !== undefined) {
+    	if(options.child) {
+    		generate.create(modulename, name, 'child');
+    	}
+    	else
+    	{
+    		generate.create(modulename, name, 'parent');
+    	}
+    }
+    else
+    {
+    	console.log('please provide required options');
+    }
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log();
+    console.log('    $ react-cli generate core helloworld -p');
+    console.log('    $ react-cli generate core hello -c');
+    console.log();
+  });  
+
+
 
 /**
  * parse commander object
