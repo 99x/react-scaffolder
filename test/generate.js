6cf403e4b8ee215209209a05fd902da3de33df0f
rajikaimal <inbox.rajika@gmail.com>
2016-09-02 15:28:13 +0530

    refactor createComponent tests

--- test/generate.js
+++ test/generate.js
@@ -1,23 +1,35 @@
 const assert = require('chai').assert;
-const create = require('../lib/generate').create;
+const createComponent = require('../lib/generate').createComponent;
 
 describe('Create react components', function() {
 	it('should create a parent react component', function(done) {
-	  create('core', 'sample', 'child', function(res) {
-	    assert.equal(res, true);
-	    done();
-	  });
+		const answers = {
+			"componentType": "child",
+			"propTypes": "yes",
+			"propNo": "2",
+			"propName": "first",
+			"propType": "number"
+		};
+	  const res = createComponent('core', 'sample', answers);
+	  assert.equal(res, true);
+	  done();
 	});
 	it('should not create a parent react component', function(done) {
-	  create('core', 'sample', 'somethingelse', function(res) {
-	    assert.equal(res, false);
-	    done();
-	  });
+	  const answers = {};
+	  const res = createComponent('core', 'sample', answers);
+	  assert.equal(res, false);
+	  done();
 	});
 	it('should not create a react component', function(done) {
-	  create('non-existing-module', 'sample', 'parent', function(res) {
-	    assert.equal(res, false);
-	    done();
-	  });
+		const answers = {
+			"componentType": "child",
+			"propTypes": "yes",
+			"propNo": "2",
+			"propName": "first",
+			"propType": "number"
+		};
+	  const res = createComponent('non-existing-module', 'sample', answers);
+	  assert.equal(res, false);
+	  done();
 	});
 });
