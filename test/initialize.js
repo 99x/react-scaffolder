add7f34f532095a32890779c1ab46723e925e41e
rajikaimal <inbox.rajika@gmail.com>
2017-04-18 20:05:13 +0530

    Make it prettier with prettier

--- test/initialize.js
+++ test/initialize.js
@@ -1,14 +1,14 @@
-const assert = require('chai').assert;
-const initApp = require('../lib/init');
-const rmdir = require('rimraf');
+const assert = require("chai").assert;
+const initApp = require("../lib/init");
+const rmdir = require("rimraf");
 
 init = new initApp();
 
-describe('Initialize React application', function() {
-	it('should initialize react app', function(done) {
-		init.initialize('../test-project', undefined, function(result) {
-			rmdir('../test-project', err => {
-				if(err) throw new Error('failed');
+describe("Initialize React application", function() {
+	it("should initialize react app", function(done) {
+		init.initialize("../test-project", undefined, function(result) {
+			rmdir("../test-project", err => {
+				if (err) throw new Error("failed");
 				else {
 					assert.equal(result, true);
 					done();
@@ -17,13 +17,13 @@ describe('Initialize React application', function() {
 		});
 	});
 
-	it('should initialize react app with eslint configuration', function(done) {
-		init.initialize('../test-project', true, function(result) {
-			rmdir('../test-project', err => {
-				if(err) throw new Error('failed');
+	it("should initialize react app with eslint configuration", function(done) {
+		init.initialize("../test-project", true, function(result) {
+			rmdir("../test-project", err => {
+				if (err) throw new Error("failed");
 				else {
 					assert.equal(result, true);
-          done();
+					done();
 				}
 			});
 		});
