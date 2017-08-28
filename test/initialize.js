3a97c56b3b1b54fede67b7700aabbdf6f498d78c
rajikaimal <inbox.rajika@gmail.com>
2017-08-28 15:40:25 +0530

    Fix unit test

--- test/initialize.js
+++ test/initialize.js
@@ -30,7 +30,7 @@ describe("Initialize React application", function () {
 	});
 
 	it("should initialize react app with git repository without eslint configuration", function (done) {
-		init.initialize("../test-project", 'https://github.com/react-boilerplate/react-boilerplate', undefined, function (result) {
+		init.initialize("../test-project", undefined, undefined, function (result) {
 			rmdir("../test-project", err => {
 				if (err) throw new Error("failed");
 				else {
