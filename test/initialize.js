3d1ae373c1545cb76c56ccd7a827d7299ecd85db
rajikaimal <inbox.rajika@gmail.com>
2017-08-28 15:51:55 +0530

    Add timeout

--- test/initialize.js
+++ test/initialize.js
@@ -1,6 +1,7 @@
 const assert = require("chai").assert;
 const initApp = require("../lib/init");
 const rmdir = require("rimraf");
+const nock = require("nock");
 
 init = new initApp();
 
@@ -30,7 +31,7 @@ describe("Initialize React application", function () {
 	});
 
 	it("should initialize react app with git repository without eslint configuration", function (done) {
-		init.initialize("../test-project", undefined, undefined, function (result) {
+		init.initialize("../test-project", 'https://github.com/react-boilerplate/react-boilerplate', undefined, function (result) {
 			rmdir("../test-project", err => {
 				if (err) throw new Error("failed");
 				else {
