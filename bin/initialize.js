15e387d3d88ffba2178f0b11f52c7fe67beefcdb
Yury Shapkarin <yury@shapkarin.me>
2019-01-07 12:17:10 +0300

    help text

--- bin/initialize.js
+++ bin/initialize.js
@@ -178,7 +178,7 @@ program
 						{
 							type: "input",
 							name: "propNames",
-							message: 'Prop names. (if name is required place "*" at the end)',
+							message: 'Prop names (if name is required place "*" at the end)',
 							paginated: true,
 							when: function(answer) {
 								return answer.propTypes === "yes";
