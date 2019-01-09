e116a496ec5d6c795d3d036fa12a398dcd0f3908
Yury Shapkarin <yury@shapkarin.me>
2019-01-10 01:42:44 +0300

    much much better regexp

--- bin/initialize.js
+++ bin/initialize.js
@@ -221,9 +221,9 @@ program
 
               for (prop of propNames){
                 if(/:/.test(prop)){
-                  const match = prop.match(/^[a-z0-9]+|(?<=:).[a-z0-9]+|\*/ig);
-                  const key = match[0] + (match[2] ? '*' : '');
-                  result[key] = match[1]
+                  const [name, type, required ] = prop.match(/^[a-z0-9]+|(?<=:).[a-z0-9]+|\*/ig);
+                  const key = name + (required ? '*' : '');
+                  result[key] = type
                 }
               }
 
