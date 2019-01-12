d5c2af37b7ccf91fd2c93fa8668cb7348f0ee517
Yury Shapkarin <yury@shapkarin.me>
2019-01-12 23:36:48 +0300

    asterisk at the end

--- bin/initialize.js
+++ bin/initialize.js
@@ -220,7 +220,7 @@ program
 
               for (prop of propNames){
                 if(/:/.test(prop)){
-                  const [name, type, required] = prop.match(/^[a-z0-9]+|(?<=:).[a-z0-9]+|\*/ig);
+                  const [name, type, required] = prop.match(/^[a-z0-9]+|(?<=:).[a-z0-9]+|\*$/ig);
                   const key = name + (required ? '*' : '');
                   withTypes[key] = type
                 }
